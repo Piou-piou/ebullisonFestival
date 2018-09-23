a4a81ef5e134a953bd6c2daac84daca2237a9b64
Anthony Pilloud <pilloud.anthony@gmail.com>
2018-09-23 12:03:30 +0200

    [func] add all files of ebullison website

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var Encore = require('@symfony/webpack-encore');
+const path = require('path');
+var glob = require('glob');
+
+Encore
+.setOutputPath('public/build/')
+.setPublicPath('/build')
+.cleanupOutputBeforeBuild()
+
+.addEntry('js/vendor', [
+  './assets/js/index.js',
+])
+
+.addEntry('js/index', [
+  './node_modules/ribs-popup/dist/js/ribs-popup.js',
+])
+
+.addStyleEntry('css/vendor', [
+  './node_modules/ribs-popup/dist/css/style.css',
+])
+
+.addStyleEntry('css/style', [
+  './assets/scss/style.scss'
+])
+
+.enableBuildNotifications()
+.enableSourceMaps(!Encore.isProduction())
+.enableVersioning(Encore.isProduction())
+
+.addLoader({
+  test: /\.js$/,
+  exclude: /node_modules/,
+  include: [
+    path.join(__dirname, 'assets/'),
+  ],
+  loader: 'babel-loader',
+})
+
+.enableSassLoader()
+
+.enableSourceMaps(!Encore.isProduction())
+;
+
+module.exports = Encore.getWebpackConfig();
